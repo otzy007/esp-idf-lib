139f05cb96387f8ddd53c47df04e5d4b7ef38f06
Andrei Gliga <otzy_007@yahoo.com>
2020-10-03 22:44:57 +0300

    LCD code

--- examples/bmp280/main/main.c
+++ examples/bmp280/main/main.c
@@ -19,6 +19,8 @@
 #include <lwip/netdb.h>
 #include "addr_from_stdin.h"
 
+#include "tftspi.h"
+#include "tft.h"
 // #if defined(CONFIG_IDF_TARGET_ESP8266)
 // #define SDA_GPIO 4
 // #define SCL_GPIO 5
@@ -38,6 +40,47 @@
 
 static const char *TAG = "example";
 char payload[150];
+
+#define SPI_BUS TFT_HSPI_HOST
+
+static char tmp_buff[64];
+
+//---------------------
+static void _dispTime()
+{
+	Font curr_font = tft_cfont;
+    // if (tft_width < 240) TFT_setFont(DEF_SMALL_FONT, NULL);
+	// else TFT_setFont(DEFAULT_FONT, NULL);
+	TFT_setFont(DEFAULT_FONT, NULL);
+	sprintf(tmp_buff, "TESTEZ SI EU");
+	TFT_print(tmp_buff, CENTER, tft_height-TFT_getfontheight()-5);
+
+    tft_cfont = curr_font;
+}
+//---------------------------------
+static void disp_header(char *info)
+{
+	TFT_fillScreen(TFT_BLACK);
+	TFT_resetclipwin();
+
+	tft_fg = TFT_YELLOW;
+	tft_bg = (color_t){ 64, 64, 64 };
+
+    // if (tft_width < 240) TFT_setFont(DEF_SMALL_FONT, NULL);
+	// else TFT_setFont(DEFAULT_FONT, NULL);
+	TFT_setFont(UBUNTU16_FONT, NULL);
+	TFT_fillRect(0, 0, tft_width-1, TFT_getfontheight()+8, tft_bg);
+	TFT_drawRect(0, 0, tft_width-1, TFT_getfontheight()+8, TFT_CYAN);
+
+	TFT_fillRect(0, tft_height-TFT_getfontheight()-9, tft_width-1, TFT_getfontheight()+8, tft_bg);
+	TFT_drawRect(0, tft_height-TFT_getfontheight()-9, tft_width-1, TFT_getfontheight()+8, TFT_CYAN);
+
+	TFT_print(info, CENTER, 4);
+	_dispTime();
+
+	tft_bg = TFT_BLACK;
+	TFT_setclipwin(0,TFT_getfontheight()+9, tft_width-1, tft_height-TFT_getfontheight()-10);
+}
 
 struct SensorData {
   float temperature;
@@ -153,6 +196,18 @@ void bmp280_test(void *pvParamters)
             sensor_data.temperature = temperature;
             sensor_data.humidity = humidity;
             xTaskCreate(udp_client_task, "udp_client", 4096, &sensor_data, 5, NULL);
+
+            TFT_print("           ", CENTER, CENTER-TFT_getfontheight() - 15);
+            sprintf(tmp_buff, "P: %.2f Pa", pressure);
+            TFT_print(tmp_buff, CENTER, CENTER-TFT_getfontheight() - 15);
+
+            TFT_print("           ", CENTER, CENTER);
+            sprintf(tmp_buff, "T: %.2f C\nH: %.2f", temperature, humidity);
+            TFT_print(tmp_buff, CENTER, CENTER);
+
+            // TFT_print("           ", CENTER, CENTER + 10);
+            // sprintf(tmp_buff, "H: %.2f", humidity);
+            // TFT_print(tmp_buff, CENTER, CENTER + 10);
         }
         else
             printf("\n");
@@ -164,6 +219,89 @@ void bmp280_test(void *pvParamters)
 
 void app_main()
 {
+    esp_err_t ret;
+
+    // === SET GLOBAL VARIABLES ==========================
+
+    // ===================================================
+    // ==== Set maximum spi clock for display read    ====
+    //      operations, function 'find_rd_speed()'    ====
+    //      can be used after display initialization  ====
+    tft_max_rdclock = 8000000;
+    // ===================================================
+
+     // ====================================================================
+     // === Pins MUST be initialized before SPI interface initialization ===
+     // ====================================================================
+     TFT_PinsInit();
+
+     // ====  CONFIGURE SPI DEVICES(s)  ====================================================================================
+
+     spi_lobo_device_handle_t spi;
+
+     spi_lobo_bus_config_t buscfg={
+         .miso_io_num=PIN_NUM_MISO,				// set SPI MISO pin
+         .mosi_io_num=PIN_NUM_MOSI,				// set SPI MOSI pin
+         .sclk_io_num=PIN_NUM_CLK,				// set SPI CLK pin
+         .quadwp_io_num=-1,
+         .quadhd_io_num=-1,
+     .max_transfer_sz = 6*1024,
+     };
+     spi_lobo_device_interface_config_t devcfg={
+         .clock_speed_hz=8000000,                // Initial clock out at 8 MHz
+         .mode=0,                                // SPI mode 0
+         .spics_io_num=-1,                       // we will use external CS pin
+     .spics_ext_io_num=PIN_NUM_CS,           // external CS pin
+     .flags=LB_SPI_DEVICE_HALFDUPLEX,        // ALWAYS SET  to HALF DUPLEX MODE!! for display spi
+     };
+
+     printf("==============================\r\n\r\n");
+
+    // ==================================================================
+    // ==== Initialize the SPI bus and attach the LCD to the SPI bus ====
+
+    ret=spi_lobo_bus_add_device(SPI_BUS, &buscfg, &devcfg, &spi);
+      assert(ret==ESP_OK);
+    printf("SPI: display device added to spi bus (%d)\r\n", SPI_BUS);
+    tft_disp_spi = spi;
+
+    // ==== Test select/deselect ====
+    ret = spi_lobo_device_select(spi, 1);
+      assert(ret==ESP_OK);
+    ret = spi_lobo_device_deselect(spi);
+      assert(ret==ESP_OK);
+
+    printf("SPI: attached display device, speed=%u\r\n", spi_lobo_get_speed(spi));
+    printf("SPI: bus uses native pins: %s\r\n", spi_lobo_uses_native_pins(spi) ? "true" : "false");
+
+    printf("SPI: display init...\r\n");
+    TFT_display_init();
+
+    tft_max_rdclock = find_rd_speed();
+    printf("SPI: Max rd speed = %u\r\n", tft_max_rdclock);
+
+      // ==== Set SPI clock used for display operations ====
+    spi_lobo_set_speed(spi, DEFAULT_SPI_CLOCK);
+    printf("SPI: Changed speed to %u\r\n", spi_lobo_get_speed(spi));
+
+      printf("\r\n---------------------\r\n");
+    printf("Graphics demo started\r\n");
+    printf("---------------------\r\n");
+
+
+    tft_font_rotate = 0;
+    tft_text_wrap = 0;
+    tft_font_transparent = 0;
+    tft_font_forceFixed = 0;
+    tft_gray_scale = 0;
+    TFT_setGammaCurve(DEFAULT_GAMMA_CURVE);
+    TFT_setRotation(LANDSCAPE);
+    TFT_setFont(TOONEY32_FONT, NULL);
+    TFT_resetclipwin();
+
+    disp_header("Temp monitor");
+      tft_fg = TFT_CYAN;
+
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
