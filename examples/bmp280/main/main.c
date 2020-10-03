33279de35e4b4aacc716de23dd12df2e1bb36ed2
Andrei Gliga <otzy_007@yahoo.com>
2020-10-03 21:39:27 +0300

    send data to telegraf

--- examples/bmp280/main/main.c
+++ examples/bmp280/main/main.c
@@ -4,14 +4,120 @@
 #include <esp_system.h>
 #include <bmp280.h>
 #include <string.h>
+#include <sys/param.h>
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
 
-#if defined(CONFIG_IDF_TARGET_ESP8266)
-#define SDA_GPIO 4
-#define SCL_GPIO 5
-#else
-#define SDA_GPIO 16
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+#include "addr_from_stdin.h"
+
+// #if defined(CONFIG_IDF_TARGET_ESP8266)
+// #define SDA_GPIO 4
+// #define SCL_GPIO 5
+// #else
+#define SDA_GPIO 15
 #define SCL_GPIO 17
+// #endif
+#if defined(CONFIG_EXAMPLE_IPV4)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
+#elif defined(CONFIG_EXAMPLE_IPV6)
+#define HOST_IP_ADDR CONFIG_EXAMPLE_IPV6_ADDR
+#else
+#define HOST_IP_ADDR ""
 #endif
+
+#define PORT CONFIG_EXAMPLE_PORT
+
+static const char *TAG = "example";
+char payload[150];
+
+struct SensorData {
+  float temperature;
+  float humidity;
+  float pressure;
+} sensor_data;
+
+static void udp_client_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char host_ip[] = HOST_IP_ADDR;
+    int addr_family = 0;
+    int ip_protocol = 0;
+    printf("!!!!!pvParamters temp: %.2f\n", sensor_data.temperature);
+#if defined(CONFIG_EXAMPLE_IPV4)
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    addr_family = AF_INET;
+    ip_protocol = IPPROTO_IP;
+#elif defined(CONFIG_EXAMPLE_IPV6)
+    struct sockaddr_in6 dest_addr = { 0 };
+    inet6_aton(HOST_IP_ADDR, &dest_addr.sin6_addr);
+    dest_addr.sin6_family = AF_INET6;
+    dest_addr.sin6_port = htons(PORT);
+    dest_addr.sin6_scope_id = esp_netif_get_netif_impl_index(EXAMPLE_INTERFACE);
+    addr_family = AF_INET6;
+    ip_protocol = IPPROTO_IPV6;
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+    struct sockaddr_in6 dest_addr = { 0 };
+    ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));
+#endif
+
+    int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+    if (sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        return;
+    }
+    ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT);
+
+    while (1) {
+        sprintf(payload, "bmp280,bmp=1 temperature=%.2f,pressure=%.2f,humidity=%.2f", sensor_data.temperature, sensor_data.pressure, sensor_data.humidity);
+        int err = sendto(sock, payload, strlen(payload), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Message sent");
+
+        struct sockaddr_in source_addr; // Large enough for both IPv4 or IPv6
+        socklen_t socklen = sizeof(source_addr);
+        int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+
+        // Error occurred during receiving
+        if (len < 0) {
+            ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+            break;
+        }
+        // Data received
+        else {
+            rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+            ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
+            ESP_LOGI(TAG, "%s", rx_buffer);
+            if (strncmp(rx_buffer, "OK: ", 4) == 0) {
+                ESP_LOGI(TAG, "Received expected message, reconnecting");
+                break;
+            }
+        }
+        break; // just go out
+        vTaskDelay(20000 / portTICK_PERIOD_MS);
+    }
+
+    if (sock != -1) {
+        ESP_LOGE(TAG, "Shutting down socket and restarting...");
+        shutdown(sock, 0);
+        close(sock);
+    }
+  vTaskDelete(NULL);
+}
 
 void bmp280_test(void *pvParamters)
 {
@@ -30,7 +136,6 @@ void bmp280_test(void *pvParamters)
 
     while (1)
     {
-        vTaskDelay(500 / portTICK_PERIOD_MS);
         if (bmp280_read_float(&dev, &temperature, &pressure, &humidity) != ESP_OK)
         {
             printf("Temperature/pressure reading failed\n");
@@ -42,16 +147,33 @@ void bmp280_test(void *pvParamters)
          * example. see sdkconfig.defaults.esp8266
          */
         printf("Pressure: %.2f Pa, Temperature: %.2f C", pressure, temperature);
-        if (bme280p)
+        if (bme280p) {
             printf(", Humidity: %.2f\n", humidity);
+            sensor_data.pressure = pressure;
+            sensor_data.temperature = temperature;
+            sensor_data.humidity = humidity;
+            xTaskCreate(udp_client_task, "udp_client", 4096, &sensor_data, 5, NULL);
+        }
         else
             printf("\n");
+
+        vTaskDelay(50000 / portTICK_PERIOD_MS);
+
     }
 }
 
 void app_main()
 {
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
     ESP_ERROR_CHECK(i2cdev_init());
     xTaskCreatePinnedToCore(bmp280_test, "bmp280_test", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, APP_CPU_NUM);
 }
-
